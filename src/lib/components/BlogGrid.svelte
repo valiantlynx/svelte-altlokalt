41c0eecd201c0d4b4d78611f077daa6992061d19
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-02-19 16:45:28 +0100

    ok entitis page is done

--- src/lib/components/BlogGrid.svelte
+++ src/lib/components/BlogGrid.svelte
@@ -1,16 +1,23 @@
 <script>
 	import EntityCards from '$lib/components/EntityCards.svelte';
 	import { page } from '$app/stores';
+
+
 	const entities = $page.data.entities;
+	const prev = entities._links.prev?.href.split("?")
+	const next = entities._links.next?.href.split("?")
 </script>
 
-<div class="flex flex-wrap">
+<div class="flex flex-wrap justify-center">
 	{#each entities._embedded.enheter as entity (entity.id)}
 		<EntityCards entity={entity} />
 	{/each}
 </div>
-<!-- if they want to see more available to go to the features page -->
-<div class="justify-center mb-8">
-	<a href="/blogs" class="flex btn btn-primary mx-8">show more</a>
+<div class="join flex flex-wrap justify-center mt-4 gap-4">
+	{#if entities._links.prev}
+		<a href={`/enheter?${prev[1]}`} class="join-item btn btn-outline">Previous</a>
+	{/if}
+	{#if entities._links.next}
+		<a href={`/enheter?${next[1]}`} class="join-item btn btn-outline">Next</a>
+	{/if}
 </div>
-
--- src/routes/enheter/+page.server.js
+++ src/routes/enheter/+page.server.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 import { error } from '@sveltejs/kit';
 
-export const load = async () => {
+export const load = async ({ url }) => {
+  // Access query parameters
+  const page = url.searchParams.get('page') || '1'; // Default to page 1 if not specified
+  const size = url.searchParams.get('size') || '30'; // Default to size 30 if not specified
+
   try {
     const apiUrl = 'https://data.brreg.no/enhetsregisteret/api/enheter';
     const response = await axios.get(apiUrl, {
       params: {
-        size: 50
+        page, // Use the page variable from the query parameters
+        size  // Use the size variable from the query parameters
       }
     });
 
@@ -16,6 +21,6 @@ export const load = async () => {
     };
   } catch (e) {
     console.error('Error: ', e);
-	throw error(e.status, e.message);
+    throw error(e.response?.status || 500, e.message);
   }
 };
