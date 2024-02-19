aa0ca61ee4afe290ecb21c4a3e624268cff21b99
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-02-19 01:03:35 +0100

    initial commit

---
+++ setup.sh
@@ -0,0 +1,3 @@
+git subtree add --prefix=apps/svelte-altlokalt https://github.com/valiantlynx/svelte-altlokalt.git main --squash
+git subtree pull --prefix=apps/svelte-altlokalt https://github.com/valiantlynx/svelte-altlokalt.git main --squash
+git subtree push --prefix=apps/svelte-altlokalt https://github.com/valiantlynx/svelte-altlokalt.git main
