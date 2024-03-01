f378f44d28bf54803a418e1f18fa93fd7dffb4a9
valiantlynx <gormerykombo@gmail.com>
2024-03-01 06:03:03 +0100

    eip

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,12 +13,18 @@ module "sg" {
     vpc_id = module.vpc.vpc_id
 }
 
+
 module "ec2" {
   source = "./modules/ec2"
   sg_id = module.sg.sg_id
   subnets = module.vpc.subnet_ids
   key_name = module.pk.key_name
   private_key_path = module.pk.private_key_path
+}
+
+module "eip" {
+  source      = "./modules/eip"
+  instance_id = module.ec2.instances
 }
 
 module "alb" {
--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_instance" "web" {
   count = length(var.ec2_names)
   ami           = data.aws_ami.ubuntu.id
