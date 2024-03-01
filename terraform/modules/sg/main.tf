af7bd01aceeae50bdc1e54040f4799436e32bb78
valiantlynx <gormerykombo@gmail.com>
2024-03-01 05:21:55 +0100

    https

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -1,12 +1,20 @@
 resource "aws_security_group" "sg" {
   name        = "sg"
-  description = "Allow HTTP, SSH inbound traffic"
+  description = "Allow HTTP, SSH, and HTTPS inbound traffic"
   vpc_id      = var.vpc_id
 
   ingress {
     description      = "HTTP"
     from_port        = 80
     to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
